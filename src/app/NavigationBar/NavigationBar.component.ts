282ed03b803cb2d99c3b0d08c8049df7ef8890f0
ChhithVattana <abcde09828@gmail.com>
2023-02-21 21:58:15 +0700

    Set Up Already

---
+++ src/app/NavigationBar/NavigationBar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-NavigationBar',
+  templateUrl: './NavigationBar.component.html',
+  styleUrls: ['./NavigationBar.component.css']
+})
+export class NavigationBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './NavigationBar/NavigationBar.component';
+import { FooterComponent } from './Footer/Footer.component';
+import { HomePageComponent } from './HomePage/HomePage.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    FooterComponent,
+    HomePageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes, {
+      useHash: true,
+      anchorScrolling: 'enabled',
+      scrollPositionRestoration: 'enabled',
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
